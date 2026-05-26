b1884c597d03b22c51eeb255b927d62002f5a5ab
shivam-dave12 <srdave12499@gmail.com>
2026-05-26 22:54:48 +0530

    rmed

--- dashboard/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Read-only operational dashboard for the institutional platform."""
-from dashboard.server import DashboardServer
-__all__ = ["DashboardServer"]
--- exchanges/groww/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Groww Trading API integration package."""
-
-from .api import GrowwRestClient
-from .data_manager import GrowwOptionDataManager
-from .underlying_data_manager import GrowwUnderlyingDataManager
-
-__all__ = ["GrowwRestClient", "GrowwOptionDataManager", "GrowwUnderlyingDataManager"]
